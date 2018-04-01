667d7d085a76995e1b31eba8970da1db490fc831
Ivan Egorov <ezone225@gmail.com>
2018-04-01 22:24:02 +0700

    proload news

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <h1 class="news-page__title">Новости БКС Экспресс</h1>
     <div class="news-page__content">
       <NewsList :news="news" />
-      <AppButton @click.native="showMoreNews">
+      <AppButton
+        @click.native="showMoreNews"
+        @mouseover.native="preloadhNews">
         <span v-if="loading">загрузка...</span>
         <span v-else>Показать ещё</span>
       </AppButton>
@@ -41,6 +43,13 @@ export default {
       await this.$store.dispatch('News/fetchNews', nextPageNumber);
       this.currentPageNumber = nextPageNumber;
       this.loading = false;
+    },
+    preloadhNews() {
+      if (this.$store.getters['News/preloadedNews'].length) {
+        return;
+      }
+      const nextPageNumber = this.currentPageNumber + 1;
+      this.$store.dispatch('News/prefetchNews', nextPageNumber);
     }
   }
 };
--- store/News.js
+++ store/News.js
@@ -1,19 +1,32 @@
 export default {
   state() {
     return {
-      news: []
+      news: [],
+      preloadedNews: []
     };
   },
   getters: {
-    news: ({ news }) => news
+    news: ({ news }) => news,
+    preloadedNews: ({ preloadedNews }) => preloadedNews
   },
   mutations: {
     addNews(state, news) {
       state.news.push(...news);
+    },
+    addPreloadedNews(state, news) {
+      state.preloadedNews.push(...news);
+    },
+    resetPreloadedNews(state, news) {
+      state.preloadedNews = [];
     }
   },
   actions: {
-    async fetchNews({ commit }, pageNumber) {
+    async fetchNews({ commit, state }, pageNumber) {
+      if (state.preloadedNews.length) {
+        commit('addNews', state.preloadedNews);
+        commit('resetPreloadedNews');
+        return;
+      }
       try {
         const news = await this.$axios.$get('/express_articles/v1', {
           params: {
@@ -23,6 +36,20 @@ export default {
           }
         });
         commit('addNews', news);
+      } catch (error) {
+        console.error('Ошибка получения новостей', error);
+      }
+    },
+    async prefetchNews({ commit }, pageNumber) {
+      try {
+        const news = await this.$axios.$get('/express_articles/v1', {
+          params: {
+            text_length: 50,
+            page: pageNumber,
+            per_page: 5
+          }
+        });
+        commit('addPreloadedNews', news);
       } catch (error) {
         console.error('Ошибка получения новостей', error);
       }
