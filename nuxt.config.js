3f2e26c1080cfefdd5907fc5927eafa0da6474cd
Ivan Egorov <ezone225@gmail.com>
2018-04-01 20:33:29 +0700

    add favicon and meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,54 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title:
+      'БКС Экспресс - новости фондового рынка и экономики. Статьи об инвестициях и финансовых инструментах. Котировки акций, графики онлайн. Технический и фундаментальный анализ. Комментарии и прогнозы аналитиков',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'БКС Экспресс - новости фондового рынка и экономики. Статьи об инвестициях и финансовых инструментах. Котировки акций, графики онлайн. Технический и фундаментальный анализ. Комментарии и прогнозы аналитиков.'
+      },
+      {
+        name: 'keywords',
+        content: 'новости, фондовый, рынок, экономика, инвестиции, финансы'
+      },
+      { property: 'og:type', content: 'website' },
+      {
+        property: 'og:title',
+        content: 'БКС Экспресс - новости фондового рынка и экономики.'
+      },
+      {
+        property: 'og:description',
+        content:
+          'Статьи об инвестициях и финансовых инструментах. Котировки акций, графики онлайн. Технический и фундаментальный анализ. Комментарии и прогнозы аналитиков.'
+      },
+      { name: 'msapplication-TileColor', content: '#2b5797' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
